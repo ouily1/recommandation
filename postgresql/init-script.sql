734f6ddd3fed80f50ecca184ece755204ce6d57b
ouily1 <>
2024-01-05 14:31:07 +0100

    ajout des fichiers postgresql

---
+++ postgresql/init-script.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE recommandations;
+\c recommandations
+CREATE SEQUENCE user_id_sequence START WITH 162542;
+CREATE TABLE "Users" (
+    "UserId" SERIAL PRIMARY KEY,
+    "Username" VARCHAR(255) NOT NULL,
+    "Password" VARCHAR(256) NOT NULL
+);
+ALTER TABLE "Users" ALTER COLUMN "UserId" SET DEFAULT nextval('user_id_sequence');
+CREATE TABLE "Movies" (
+    "MovieId" SERIAL PRIMARY KEY,
+    "Title" VARCHAR(255),
+    "Genres" VARCHAR(255),
+    "Year" INTEGER,
+    "Synopsis" TEXT,
+    "Affiche" TEXT
+);
+CREATE TABLE "Ratings" (
+    "UserId" INTEGER,
+    "MovieId" INTEGER,
+    "Rating" NUMERIC(2, 1),
+    PRIMARY KEY ("UserId", "MovieId"),
+    FOREIGN KEY ("UserId") REFERENCES "Users" ("UserId"),
+    FOREIGN KEY ("MovieId") REFERENCES "Movies" ("MovieId")
+);
+\c recommandations
+COPY "Movies" FROM '/tmp/db_sql.csv' DELIMITER ',' CSV HEADER;
